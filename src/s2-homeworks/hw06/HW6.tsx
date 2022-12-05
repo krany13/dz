ca577fee39cf12436c4dd43ed03d1c34deb1d440
krany13 <krasniy.vladim@yandex.ru>
2022-12-05 23:54:35 +0300

    try to fox HW6

--- src/s2-homeworks/hw06/HW6.tsx
+++ src/s2-homeworks/hw06/HW6.tsx
@@ -19,7 +19,8 @@ const HW6 = () => {
     }
     const restore = () => {
         // делают студенты
-
+        // достать из localStorage
+        setValue(restoreState('hw6-editable-span-value', ""))
     }
 
     return (
