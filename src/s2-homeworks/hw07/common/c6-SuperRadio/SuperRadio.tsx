7f49fd85aa7e0e7973e89ea9ed26f8fb2a04a96e
krany13 <krasniy.vladim@yandex.ru>
2022-12-07 03:26:08 +0300

    try to fox HW7

--- src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
+++ src/s2-homeworks/hw07/common/c6-SuperRadio/SuperRadio.tsx
@@ -36,6 +36,7 @@ const SuperRadio: React.FC<SuperRadioPropsType> = ({
 }) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
         // делают студенты
+        onChangeOption?.(e.currentTarget.name)
     }
 
     const finalRadioClassName = s.radio + (className ? ' ' + className : '')
