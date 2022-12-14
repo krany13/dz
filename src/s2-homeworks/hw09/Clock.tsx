c460441b268182fb98041a5967922b6d0d4959af
krany13 <krasniy.vladim@yandex.ru>
2022-12-14 15:36:11 +0300

    fix HW9 v7 (date.month+day+year)

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -39,7 +39,7 @@ function Clock() {
     const newDate = new Date()
     const dataView = `${newDate.getHours()}:${newDate.getMinutes()}:${newDate.getSeconds()}`
     const stringTime = dataView || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const dataYear = `${newDate.getDay()}/${newDate.getMonth()}/${newDate.getFullYear()}`
+    const dataYear = `${newDate.getDate()}/${newDate.getMonth()}/${newDate.getFullYear()}`
     const stringDate = dataYear || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
