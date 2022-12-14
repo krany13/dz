2d01e6861aeac467ff7ae5d2d84423c714c75f3d
krany13 <krasniy.vladim@yandex.ru>
2022-12-14 16:15:34 +0300

    fix HW9 v10 button

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -91,7 +91,7 @@ function Clock() {
                 </SuperButton>
                 <SuperButton
                     id={'hw9-button-stop'}
-                    disabled={!!timerId} // пишут студенты // задизэйблить если таймер не запущен
+                    disabled={!timerId} // пишут студенты // задизэйблить если таймер не запущен
                     onClick={stop}
                 >
                     stop
